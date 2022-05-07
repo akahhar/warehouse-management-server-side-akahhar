190f787cd1a01137d5bed551ba6de811cfd04450
akahhar <nayeem.edu@gmail.com>
2022-05-07 16:26:07 +0600

    new commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // DB_USER=geniusUser
 // DB_PASS=eJIqqo7CFJm00v7o
 
-const uri = `mongodb+srv://geniusUser:eJIqqo7CFJm00v7o@cluster0.tyesc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tyesc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
